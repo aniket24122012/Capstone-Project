681260c0dc7790e4aa51da9eadd4cc32ecfa1fb1
aniket24122012 <aniketkandrikar2000@gmail.com>
2025-10-07 06:05:15 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description='Its a final project for mlops',
+    description='abc',
     author='Aniket Kandrikar',
     license='MIT',
 )
