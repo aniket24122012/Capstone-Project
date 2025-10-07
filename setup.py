bae677649fdab8dee4998e7eca20dd28336b6bc3
aniket24122012 <aniketkandrikar2000@gmail.com>
2025-10-07 05:59:37 -0700

    ready for killerconda

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description='It's a final project for mlops',
+    description='Its a final project for mlops',
     author='Aniket Kandrikar',
     license='MIT',
 )
